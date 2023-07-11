a81ea5a5fce81ede5c736bca6e565510ad620f7c
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-07-11 15:09:15 -0500

    fix(PetShop):Refactor application. Separate in Controller, Service, and Repository using annotations and dependency injection

--- src/main/java/com/example/PetShop/PetShopApplication.java
+++ src/main/java/com/example/PetShop/PetShopApplication.java
@@ -1,25 +1,26 @@
 package com.example.PetShop;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import com.example.PetShop.Entities.Pet;
+import com.example.PetShop.Entities.Tutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import com.example.PetShop.services.PetService;
+import com.example.PetShop.services.TutorService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class PetShopApplication {
 
-	private Map<String, Tutor> tutors = new HashMap<>();
-	private Map<String, Pet> pets = new HashMap<>();
+	private final TutorService tutorService;
+	private final PetService petService;
+
+	public PetShopApplication(TutorService tutorService, PetService petService) {
+		this.tutorService = tutorService;
+		this.petService = petService;
+	}
 
 	@GetMapping("/")
 	public String home() {
@@ -30,82 +31,58 @@ public class PetShopApplication {
 
 	@GetMapping("/tutors")
 	public List<Tutor> getAllTutors() {
-		return new ArrayList<>(tutors.values());
+		return tutorService.getAllTutors();
 	}
 
 	@GetMapping("/tutors/{id}")
 	public Tutor getTutor(@PathVariable String id) {
-		return tutors.get(id);
+		return tutorService.getTutor(id);
 	}
 
 	@PostMapping("/tutors")
 	public Tutor createTutor(@RequestBody Tutor tutor) {
-		tutors.put(tutor.getId(), tutor);
-		return tutor;
+		return tutorService.createTutor(tutor);
 	}
 
 	@PutMapping("/tutors/{id}")
 	public Tutor updateTutor(@PathVariable String id, @RequestBody Tutor tutor) {
-		tutors.put(id, tutor);
-		return tutor;
+		return tutorService.updateTutor(id, tutor);
 	}
 
 	@DeleteMapping("/tutors/{id}")
 	public void deleteTutor(@PathVariable String id) {
-		tutors.remove(id);
+		tutorService.deleteTutor(id);
 	}
 
 	// Pet Endpoints
 
 	@GetMapping("/pets")
 	public List<Pet> getAllPets() {
-		return new ArrayList<>(pets.values());
+		return petService.getAllPets();
 	}
 
 	@GetMapping("/pets/{id}")
 	public Pet getPet(@PathVariable String id) {
-		return pets.get(id);
+		return petService.getPet(id);
 	}
 
 	@PostMapping("/pets")
 	public Pet createPet(@RequestBody Pet pet) {
-		pets.put(pet.getId(), pet);
-		return pet;
+		return petService.createPet(pet);
 	}
 
 	@PutMapping("/pets/{id}")
 	public Pet updatePet(@PathVariable String id, @RequestBody Pet pet) {
-		pets.put(id, pet);
-		return pet;
+		return petService.updatePet(id, pet);
 	}
 
 	@DeleteMapping("/pets/{id}")
 	public void deletePet(@PathVariable String id) {
-		pets.remove(id);
+		petService.deletePet(id);
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetShopApplication.class, args);
 	}
-
-}
-
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-class Tutor {
-	private String id;
-	private String name;
-
-}
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-class Pet {
-	private String id;
-	private String name;
 
 }
